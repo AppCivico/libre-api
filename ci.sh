a7ef02138a55f7a58cf9c6f2527da8ddd1fa64ee
Junior Moraes <juniorfvox@gmail.com>
2017-03-29 14:44:44 -0300

    LIB-36: Continuous integration.

---
+++ ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source ~/perl5/perlbrew/etc/bashrc
+cd $DIR
+echo "doing cpanm --installdeps on $DIR"
+cpanm Module::Install::Catalyst App::Sqitch App::ForkProve -n
+sqitch deploy -t local
+cpanm -n --installdeps .
+TRACE=1 forkprove -MSaveh -j 1  -lvr t/
