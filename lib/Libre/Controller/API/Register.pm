2eb6a894f371a0e9a103e6d43471cf8d51d63583
Lucas Ansei <lucas.ansei@eokoe.com>
2017-03-30 15:02:30 -0300

    Retirando uma parte específica de jornalista do geral.
    # Conflicts:
    #	lib/Libre/Controller/API/Register.pm

---
+++ lib/Libre/Controller/API/Register.pm
@@ -0,0 +1,41 @@
+package Libre::Controller::API::Register;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+=head1 NAME
+
+Libre::Controller::API::Register - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+sub root :Chained('/api/root') :PathPart('') :CaptureArgs(0) {
+    my ($self, $c) = @_;
+}
+
+sub base :Chained('root') :PathPart('register') :CaptureArgs(0) { }
+
+=encoding utf8
+
+=head1 AUTHOR
+
+eokoe-lucas,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
