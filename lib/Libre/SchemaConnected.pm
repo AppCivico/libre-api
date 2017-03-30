3b01349db786bdaff8a148a44d494fb31a6ed233
Junior Moraes <juniorfvox@gmail.com>
2017-03-30 10:45:11 -0300

    Conexão com o banco de dados no Catalyst.

--- lib/Libre/SchemaConnected.pm
+++ lib/Libre/SchemaConnected.pm
@@ -12,20 +12,19 @@ BEGIN {
 require Exporter;
 
 our @ISA    = qw(Exporter);
-our @EXPORT = qw(get_schema);
+our @EXPORT = qw(get_schema get_connect_info);
 
 use Libre::Schema;
 use Libre::Utils;
 
-sub get_schema {
-
+sub get_connect_info {
     my $host     = $ENV{POSTGRESQL_HOST};
     my $port     = $ENV{POSTGRESQL_PORT} || 5432;
     my $user     = $ENV{POSTGRESQL_USER};
     my $password = $ENV{POSTGRESQL_PASSWORD};
     my $dbname   = $ENV{POSTGRESQL_DBNAME};
 
-    return Libre::Schema->connect({
+    return {
         dsn            => "dbi:Pg:dbname=$dbname;host=$host;port=5432",
         user           => $user,
         password       => $password,
@@ -34,7 +33,11 @@ sub get_schema {
         name_sep       => ".",
         auto_savepoint => 1,
         pg_enable_utf8 => 1,
-    });
+    };
+}
+
+sub get_schema {
+    return Libre::Schema->connect(get_connect_info());
 }
 
 1;
