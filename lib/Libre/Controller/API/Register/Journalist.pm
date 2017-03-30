c7129c47b1af6f5a9ee1910ec415fd52a6101c9b
Lucas Ansei <lucas.ansei@eokoe.com>
2017-03-30 13:13:31 -0300

    Fix: rota de controller de registro.

---
+++ lib/Libre/Controller/API/Register/Journalist.pm
@@ -0,0 +1,61 @@
+package Libre::Controller::API::Register::Journalist;
+use Moose;
+use common::sense;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+=head1 NAME
+
+Libre::Controller::API::Register::Journalist - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+sub register :Chained('register/base') :PathPart('') :Args(0) :ActionClass('REST') { }
+
+sub register_POST {
+    my ($self, $c) = @_;
+
+    my $journalist = $c->stash->{collection}->execute(
+        $c,
+        for   => 'create',
+        with  => {
+            %{ $c->req->params },
+            status => "pending",
+        },
+    );
+
+    # Enviando e-mail de confirmação
+    $journalist->send_email_registration();
+
+    $self->status_created(
+        $c,
+        location => $c->uri_for($c->controller('API::Journalist')->action_for('journalist'), [$journalist->id]),
+        entity   => { id => $journalist->id }
+    );
+}
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+eokoe-lucas,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
