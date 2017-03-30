9e3347d7e8d2ce0dbc5f1b5b514cb77fb0dc6112
Junior Moraes <juniorfvox@gmail.com>
2017-03-30 17:18:32 -0300

    Libre::Data::Verifier.

---
+++ lib/Libre/Role/Verification/TransactionalActions/DBIC.pm
@@ -0,0 +1,23 @@
+package Libre::Role::Verification::TransactionalActions::DBIC;
+
+use namespace::autoclean;
+use Moose::Role;
+
+with 'Libre::Role::Verification::TransactionalActions';
+
+has schema => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { return shift->result_source->schema }
+);
+
+sub _wrap_in_transaction {
+    my ( $self, $code ) = @_;
+    return sub {
+        my @args = @_;
+        $self->schema->txn_do( sub { $code->(@args) } );
+    };
+}
+
+1;
+
