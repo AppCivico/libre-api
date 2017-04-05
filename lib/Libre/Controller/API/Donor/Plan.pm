1c64963e3fafa3f4f280d3526af536dda8e052a8
Lucas Ansei <lucas.ansei@eokoe.com>
2017-04-05 17:07:18 -0300

    Rotas de plan.

---
+++ lib/Libre/Controller/API/Donor/Plan.pm
@@ -0,0 +1,37 @@
+package Libre::Controller::API::Donor::Plan;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+=head1 NAME
+
+Libre::Controller::API::Donor::Plan - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+eokoe-lucas,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
