f9abf5187a3e8622d7228f00a23b17a3dc08c777
Lucas Ansei <lucas.ansei@eokoe.com>
2017-04-07 19:22:41 -0300

    ResultSet para planos

---
+++ lib/Libre/Schema/ResultSet/UserPlan.pm
@@ -0,0 +1,64 @@
+package Libre::Schema::ResultSet::UserPlan;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+extends 'DBIx::Class::ResultSet';
+
+# Verificar roles
+with 'Libre::Role::Verification';
+with 'Libre::Role::Verification::TransactionalActions::DBIC';
+
+use Data::Verifier;
+
+sub verifiers_specs {
+    my $self = shift;
+
+    return {
+        create => Data::Verifier->new(
+            filters => [ qw(trim) ],
+            amount => {
+                required    => 1,
+                type        => "Num",
+                post_check  => sub {
+                    my $amount = $_[0]->get_value('amount');
+
+                    if ($amount < 20) {
+                        return 0;
+                    }
+                    return 1;
+                }
+            }
+        )
+    }
+}
+
+sub action_specs {
+    my $self = @_;
+
+    return {
+        create => sub {
+            my $r = shift;
+
+            my %values = $r->valid_values;
+            not defined $values{$_} and delete $values{$_} for keys %values;
+
+            my $user = $self->result_source->schema->resultset("User")->create({
+                ( map { $_ => $values{$_} } qw(email password) ),
+                verified    => 1,
+                verified_at => \"now()",
+            });
+
+            $user->add_to_roles({ id => 3 });
+
+            my $user_plan = $self->create({
+                ( map { $_ => $values{$_} } qw(amount) ),
+                user_id => $user->id,
+            });
+
+            return $user_plan;
+        },
+    };
+}
+
+1;
