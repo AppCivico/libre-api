ddea0683d804967ff69a4b5ab25bca493cf93cf9
Lucas Ansei <lucas.ansei@eokoe.com>
2017-04-10 10:15:01 -0300

    Esta deve ser a estrutura de diretórios correta.

--- lib/Libre/Controller/API/User/Plan.pm
+++ lib/Libre/Controller/API/User/Plan.pm
@@ -5,27 +5,36 @@ use namespace::autoclean;
 
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
-sub root :Chained('/api/root') :PathPart('user') :CaptureArgs(0) { }
+sub root :Chained('/api/user/object') :PathPart('') :CaptureArgs(0) { 
+    my ($self, $c) = @_;
+
+    eval { $c->assert_user_roles(qw/donor/) };
+    if ($@) {
+        $c->forward("/api/forbidden");
+    }
+}
  
 sub base :Chained('root') :PathPart('plan') :CaptureArgs(0) { }
 
-sub list :Chained('base') :PathPart('') :Args(0) ActionClass('REST') { }
-
-sub list_GET { 
+sub register :Chained('base') :PathPart('') :Args(0) ActionClass('REST') {
     my ($self, $c) = @_;
 
-    my $plan_rs = $c->model("DB::Plan")->search({ id => $c->user->id });
+    $c->stash->{collection} = $c->model('DB::UserPlan');
+}
 
-    return $self->status_ok(
+sub register_POST { 
+    my ($self, $c) = @_;
+
+    my $user_plan = $c->stash->{collection}->execute(
         $c,
-        entity => {
-            plan => $c->stash->{plan_rs}->search(
-            {},
-            {
-                result_class => "DBIx::Class::ResultClass::HashRefInflator",
-            }
-            )->next
-        },
+        for  => "create",
+        with => $c->req->params,
+    );
+
+    return $self->status_created(
+        $c,
+        location => $c->uri_for($c->controller("API::User::Plan")->action_for('result'), [ $user_plan->id ]),
+        entity   => { id => $user_plan->id },
     );
 }
 
