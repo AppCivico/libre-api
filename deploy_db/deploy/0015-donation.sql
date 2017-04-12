928ec480259b5061f6707217c81cdde660220947
Lucas Ansei <lucas.ansei@eokoe.com>
2017-04-12 15:36:22 -0300

    Criando tabela de doações

---
+++ deploy_db/deploy/0015-donation.sql
@@ -0,0 +1,14 @@
+-- Deploy libre:0015-donation to pg
+-- requires: 0014-userid-not-null-user-plan
+
+BEGIN;
+
+CREATE TABLE  donation
+(
+    id              SERIAL                          PRIMARY KEY,
+    user_id         integer                         NOT NULL REFERENCES donor(user_id),
+    journalist_id   integer                         NOT NULL REFERENCES journalist(user_id),
+    created_at      timestamp without time zone     NOT NULL
+)
+
+COMMIT;
