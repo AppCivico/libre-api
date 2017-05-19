a9f5c22b93c4cf1eb1191370630221ab051b2e18
Junior Moraes <juniorfvox@gmail.com>
2017-05-19 17:55:09 -0300

    source nas envs pro libre_create.pl não disparar exception.

--- script/schema.dump.sh
+++ script/schema.dump.sh
@@ -4,6 +4,8 @@ if [ -d "script" ]; then
   cd script;
 fi
 
+source ../envfile.sh
+
 perl libre_create.pl model DB DBIC::Schema Libre::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=libre_dev;host=localhost' postgres trustable quote_names=1 overwrite_modifications=1
 
 cd ..;
