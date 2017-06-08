6438cd7642f06322306e219b2cd990c19ae4a6e7
Lucas Ansei <lucas.ansei@eokoe.com>
2017-06-08 17:04:34 -0300

    Implementando dashboard de doador.

--- lib/Libre/Controller/API/Donor/Dashboard.pm
+++ lib/Libre/Controller/API/Donor/Dashboard.pm
@@ -3,7 +3,14 @@ use common::sense;
 use Moose;
 use namespace::autoclean;
 
-BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+use Libre::Utils;
+use Furl;
+use DateTime;
+
+BEGIN {
+    extends 'CatalystX::Eta::Controller::REST';
+    $ENV{LIBRE_KORDUV_URL} or die "missing env 'LIBRE_KORDUV_URL'.";
+}
 
 with "CatalystX::Eta::Controller::AutoListGET";
 
@@ -21,6 +28,10 @@ __PACKAGE__->config(
     },
 );
 
+has 'furl' => ( is => 'rw', lazy => 1, builder => '_build_furl' );
+
+sub _build_furl { Furl->new }
+
 sub root : Chained('/api/donor/object') : PathPart('')  : CaptureArgs(0) { 
     my ($self, $c) = @_;
 
@@ -36,25 +47,53 @@ sub base : Chained('root') : PathPart('dashboard') : CaptureArgs(0) {
     $c->stash->{collection} = $c->stash->{donor}->user->user_plans;
 }
 
-sub list : Chained('base') : PathPart('') : Args(1) : ActionClass('REST') { }
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
 
 sub list_GET {
-    my ($self, $c, $id) = @_;
+    my ($self, $c, %opts) = @_;
 
-    use DDP; p $id;
+    my $next_billing_at;
+    my $plan = $c->model("DB::UserPlan")->search(
+        {
+            user_id => $c->user->id,
+        }
+    )->next;
+    
+    # TODO mostrar até libres órfãos, mas avisar que não serão doados se não tiverem plano
+    my $libres = $c->model("DB::Libre")->search(
+        {
+            user_plan_id => $plan->id,
+            invalid      => 0,
+            orphaned_at  => \"IS NULL",
+        }
+    )->count;
 
-    if (defined($id) && (my $user_plan = $c->stash->{collection}->find($id))) {
-        $c->stash->{user_plan} = $user_plan;
+    if (is_test()) {
+        $next_billing_at = DateTime->now(),
     }
     else {
-        $c->detach("/error_404");
+        my $res;
+        eval {
+            retry {
+                $res = $self->furl->get( $ENV{LIBRE_KORDUV_URL} . '/subscriptions');
+                die $res->decoded_content unless $res->is_success;
+            }
+            retry_of { shift() < 3 } catch { die $_; };
+        };
+
+        die "Error: $@" if $@;
+        die "Cannot call GET on korduv" unless $res;
+        die "Request failed: " . $res->as_string unless $res->is_success;
+
+        $next_billing_at = decode_json( $res->decoded_content->status->next_billing_at );
     }
 
-    
     return $self->status_ok(
         $c,
         entity => {
-
+            user_plan_amount => $plan->amount,
+            libres_donated   => $libres,
+            next_billing_at  => $next_billing_at,
         },
     );
 }
--- t/donor/008-donor-dashboard.t
+++ t/donor/008-donor-dashboard.t
@@ -3,6 +3,7 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib";
 
 use Libre::Test::Further;
+use DateTime;
 
 my $schema = Libre->model("DB");
 
@@ -13,44 +14,44 @@ db_transaction {
 
     my $journalist_id = stash "journalist.id";
     my $donor_id      = stash "donor.id";
+    my $amount        = fake_int(2001, 100000)->();
 
-    rest_put "/api/donor/$donor_id/plan",
-        name   => "create donor plan",
-        stash  => "user_plan",
+    rest_post "/api/donor/$donor_id/plan",
+        name   => "create first donor plan",
+        stash  => "donor_plan",
         params => {
-            amount => fake_int(2001, 100000)->(),
+            amount => $amount,
         },
     ;
 
-    my $plan_id = (stash("user_plan")->{id});
+    rest_post "/api/journalist/$journalist_id/support",
+        name  => "donate to a journalist",
+    ;
 
-    rest_get "/api/donor/$donor_id/dashboard";
+    rest_post "/api/journalist/$journalist_id/support",
+        name  => "donate to a journalist",
+    ;
 
-    # my $plan_id = (stash("user_plan")->{id});
+    rest_post "/api/journalist/$journalist_id/support",
+        name  => "donate to a journalist",
+    ;
 
-    # rest_post "/api/journalist/$journalist_id/support",
-    #     name  => "donate to a journalist",
-    #     stash => "s1",
-    # ;
+    my $plan_id  = (stash("donor_plan")->{id});
 
-    # rest_get "/api/donor/$donor_id/plan/$plan_id",
-    #     name  => "counting once",
-    #     stash => "c1",
-    # ;
+    rest_get "/api/donor/$donor_id/dashboard",
+        name  => "donor dashboard",
+        stash => "donor_dashboard"
+    ;
 
-    # is((stash("c1")->{total_supports}), 1, 'One libre, as expected');
-
-    # rest_post "/api/journalist/$journalist_id/support",
-    #     name  => "donate to a journalist",
-    #     stash => "s2",
-    # ;
-
-    # rest_get "/api/donor/$donor_id/plan/$plan_id",
-    #     name  => "counting once again",
-    #     stash => "c2"
-    # ;
-
-    # is((stash("c2")->{total_supports}), 2, 'two libres, as expected');
+    is_deeply(
+        (stash "donor_dashboard"),
+        {
+            libres_donated   => 3,
+            user_plan_amount => $amount,
+            next_billing_at  => DateTime->now,
+        },
+        'Donor dashboard'
+    );
 };
 
 done_testing();
