e034c6313d62f40703927fba4d4a6d47ad59ec33
Junior Moraes <juniorfvox@gmail.com>
2017-06-12 11:06:47 -0300

    Fix: not null quebrou o homol.

--- deploy_db/deploy/0034-libre-page-data.sql
+++ deploy_db/deploy/0034-libre-page-data.sql
@@ -3,7 +3,9 @@
 
 BEGIN;
 
-alter table libre add column page_title text not null;
-alter table libre add column page_referer text not null;
+alter table libre add column page_title text not null default '';
+alter table libre add column page_referer text not null default '';
+alter table libre alter column page_title drop default;
+alter table libre alter column page_referer drop default;
 
 COMMIT;
