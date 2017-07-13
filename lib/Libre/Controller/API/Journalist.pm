831834af5f31e1f30fd0daf993daffde32d10243
Lucas Ansei <lucas.ansei@eokoe.com>
2017-07-13 15:07:13 -0300

    Fix: o detach deve ocorrer apenas no result.

--- lib/Libre/Controller/API/Journalist.pm
+++ lib/Libre/Controller/API/Journalist.pm
@@ -17,16 +17,6 @@ __PACKAGE__->config(
     # AutoResultPUT.
     object_key     => "journalist",
     result_put_for => "update",
-
-    # AutoResultGET
-    # object_key => "journalist",
-    # build_row  => sub {
-    #     my ($result) = @_;
-
-    #     return {
-    #         map { $_ => $result->get_column($_) } qw/user_id address_state address_city address_zipcode address_street address_residence_number cellphone_number/
-    #     };
-    # },
 );
 
 sub root : Chained('/api/logged') : PathPart('') : CaptureArgs(0) { }
@@ -36,25 +26,20 @@ sub base : Chained('root') : PathPart('journalist') : CaptureArgs(0) { }
 sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
     my ($self, $c, $user_id) = @_;
 
-    # my $user = $c->stash->{collection}->find($user_id);
-    # $c->detach("/error_404") unless ref $user;
-
-    # $c->stash->{journalist} = $user;
-
-    $c->stash->{collection} = $c->stash->{collection}->search( { user_id => $user_id } );
-
     my $user = $c->stash->{collection}->find($user_id);
     $c->detach("/error_404") unless ref $user;
 
     $c->stash->{is_me} = int($c->user->id == $user->id);
     $c->stash->{journalist} = $user;
+}
+
+sub result : Chained('object') : PathPart('') : Args(0) : ActionClass('REST') {
+    my ($self, $c) = @_;
 
     $c->detach("/api/forbidden") unless $c->stash->{is_me};
 }
 
-sub result : Chained('object') : PathPart('') : Args(0) : ActionClass('REST') { }
-
-sub result_GET { 
+sub result_GET {
     my ($self, $c) = @_;
 
     return $self->status_ok(
