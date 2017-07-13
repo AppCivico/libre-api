9ec51c3c9a4f64ea0a302b8e44fe0ff2bb836109
Lucas Ansei <lucas.ansei@eokoe.com>
2017-07-13 14:35:54 -0300

    Implementando edição do perfil do jornalista.

--- lib/Libre/Controller/API/Journalist.pm
+++ lib/Libre/Controller/API/Journalist.pm
@@ -6,12 +6,27 @@ use namespace::autoclean;
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
 with "CatalystX::Eta::Controller::AutoBase";
+with "CatalystX::Eta::Controller::AutoResultPUT";
 
 __PACKAGE__->config(
     # AutoBase.
     result      => "DB::Journalist",
     result_cond => { 'user.verified' => "true" },
     result_attr => { join => "user" },
+
+    # AutoResultPUT.
+    object_key     => "journalist",
+    result_put_for => "update",
+
+    # AutoResultGET
+    # object_key => "journalist",
+    # build_row  => sub {
+    #     my ($result) = @_;
+
+    #     return {
+    #         map { $_ => $result->get_column($_) } qw/user_id address_state address_city address_zipcode address_street address_residence_number cellphone_number/
+    #     };
+    # },
 );
 
 sub root : Chained('/api/logged') : PathPart('') : CaptureArgs(0) { }
@@ -21,13 +36,38 @@ sub base : Chained('root') : PathPart('journalist') : CaptureArgs(0) { }
 sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
     my ($self, $c, $user_id) = @_;
 
+    # my $user = $c->stash->{collection}->find($user_id);
+    # $c->detach("/error_404") unless ref $user;
+
+    # $c->stash->{journalist} = $user;
+
+    $c->stash->{collection} = $c->stash->{collection}->search( { user_id => $user_id } );
+
     my $user = $c->stash->{collection}->find($user_id);
     $c->detach("/error_404") unless ref $user;
 
+    $c->stash->{is_me} = int($c->user->id == $user->id);
     $c->stash->{journalist} = $user;
+
+    $c->detach("/api/forbidden") unless $c->stash->{is_me};
 }
 
 sub result : Chained('object') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub result_GET { 
+    my ($self, $c) = @_;
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            ( map { $_ => $c->stash->{journalist}->$_ } qw/address_state address_city address_zipcode address_street address_residence_number cellphone_number/ ),
+
+            ( map { $_ => $c->stash->{journalist}->user->$_ } qw/id email created_at name surname/ ),
+        }
+    );
+}
+
+sub result_PUT { }
 
 __PACKAGE__->meta->make_immutable;
 
---
+++ t/journalist/004-me.t
@@ -0,0 +1,67 @@
+use common::sense;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+use Libre::Test::Further;
+my $schema = Libre->model("DB");
+
+db_transaction {
+    create_journalist(
+        name                     => "Lucas",
+        surname                  => "Ansei",
+        address_state            => "São Paulo",
+        address_city             => "Taubaté",
+        address_zipcode          => "12082-340",
+        address_street           => "Rua Florival de Toledo",
+        address_residence_number => "1"
+    );
+
+    my $journalist_id = stash "journalist.id";
+
+    rest_get "/api/journalist/$journalist_id",
+        name    => "get when logged off --fail",
+        is_fail => 1,
+        code    => 403,
+    ;
+
+    api_auth_as user_id => $journalist_id;
+    rest_get "/api/journalist/$journalist_id",
+        name  => "get journalist",
+        list  => 1,
+        stash => "get_journalist"
+    ;
+
+    stash_test "get_journalist" => sub {
+        my $res = shift;
+
+        is($res->{id}, $schema->resultset("User")->find($journalist_id)->id);
+    };
+
+    rest_put "/api/journalist/$journalist_id",
+        name => "update journalist",
+        [ 
+            surname      => "Da Silva",
+            address_city => "Santos",
+        ]
+    ;
+
+    rest_reload_list "get_journalist";
+
+    stash_test "get_journalist.list" => sub {
+        my $res = shift;
+
+        is($res->{surname},      "Da Silva", "name updated");
+        is($res->{address_city}, "Santos",   "city updated");
+    };
+
+    create_journalist;
+    rest_get [ "api", "journalist", stash "journalist.id" ], name => "can't get other journalist", is_fail => 1, code => 403;
+    rest_put [ "api", "journalist", stash "journalist.id" ],
+        name    => "can't put other journalist",
+        is_fail => 1,
+        code    => 403,
+        [ name => fake_name()->() ]
+    ;
+};
+
+done_testing();
