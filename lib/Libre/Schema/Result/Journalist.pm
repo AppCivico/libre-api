e379498412ada417572e49d05816456a3298bff9
Junior Moraes <juniorfvox@gmail.com>
2017-07-14 16:53:24 -0300

    Fix: unused var.

--- lib/Libre/Schema/Result/Journalist.pm
+++ lib/Libre/Schema/Result/Journalist.pm
@@ -330,7 +330,7 @@ sub get_authlink {
     if (!defined($self->customer_id) && !defined($self->customer_key)) {
         my $register = $self->_picpay->register();
 
-        my $customer = $self->_picpay->customer(
+        $self->_picpay->customer(
             customer_key => $register->{customer}->{customer_key},
             customer => {
                 id_internal      => $self->id,
