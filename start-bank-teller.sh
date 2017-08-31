ea37aaf888d7658a79a9cdccd19cecf0cca66c71
Junior Moraes <juniorfvox@gmail.com>
2017-08-31 14:45:46 -0300

    Fix: logs estavam apontados pro lugar errado.

--- start-bank-teller.sh
+++ start-bank-teller.sh
@@ -2,4 +2,4 @@
 cd /src;
 source /home/app/perl5/perlbrew/etc/bashrc
 source envfile.sh
-perl script/daemon/BankTeller start -f 1>>/data/log/email.log 2>>/data/log/email.error.log
+perl script/daemon/BankTeller start -f 1>>/data/log/bankteller.log 2>>/data/log/bankteller.error.log
